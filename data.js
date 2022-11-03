13267303d39626df8fe8d247e3a7399b20f4d173
Harsh Prakash Sharma <110193200+hps542123@users.noreply.github.com>
2022-11-03 16:21:30 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Heaven',
+        des: 'Heaven is often described as a "highest place", the holiest place, a Paradise, in contrast to hell or the Underworld or the "low places"',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'liger',
+        des: 'They are large, muscly and male ligers will have a mane, like a male lion, but often shorter than their fathers. They have dark tawny fur, with broad heads. They often have feint tiger stripes, inherited from their mother.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'vikram vedha',
+        des: 'Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'The Warrior',
+        des: 'Every nerve ending and blood vessel of the film is stuck in the past". Latha Srinivasan of Firstpost rated the film 1.5 out of 5 stars and wrote "Director Lingusamys Telugu debut starring Ram Pothineni is a mish-mash of his earlier films. The Warrior loses the battle even before setting foot on the battleground".',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Babli bouncer',
+        des: 'A young sea monster boy with the ability to assume human form while on land, who explores the town of Portorosso with his new best friends, Alberto Scorfano and Giulia Marcovaldo, experiencing a life-changing summer adventure.',
+        image: 'images/slider 5.PNG'
+    }
+]
